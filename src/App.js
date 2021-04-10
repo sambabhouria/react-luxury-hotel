304abd51025559ce2417df7e3f07c4b49669ddd1
Samba Diallo <samba.diallo@id-mac007.idtgv.cnit>
2021-04-10 20:06:57 +0200

    react-luxury-hotel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ServicesPage from "./pages/ServicesPage";
+import RoomsPage from "./pages/RoomsPage";
+import SingleRoomPage from "./pages/SingleRoomPage";
+import DefaultPage from "./pages/DefaultPage";
+
+import NavButton from "./components/Globals/NavButton";
+import Navbar from "./components/Globals/Navbar";
+import { navLinks } from "./tempLinks";
+
+class App extends Component {
+  state = {
+    showNavbar: false,
+    navLinks: navLinks
+  };
+  toggleNavbar = () => {
+    this.setState({
+      showNavbar: !this.state.showNavbar
+    });
+  };
+  render() {
+    return (
+      <>
+        <NavButton toggleNavbar={this.toggleNavbar} />
+        <Navbar
+          showNavbar={this.state.showNavbar}
+          toggleNavbar={this.toggleNavbar}
+          navLinks={this.state.navLinks}
+        />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/services" component={ServicesPage} />
+          <Route path="/rooms" component={RoomsPage} />
+          <Route path="/rooms/:id" component={SingleRoomPage} />
+          <Route component={DefaultPage} />
+        </Switch>
+      </>
+    );
+  }
 }
 
 export default App;
